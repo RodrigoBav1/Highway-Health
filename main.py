1c3a01ca0cd27cd479bbb2744cf16dbbdeaa721c
bond1999 <46317327+bond1999@users.noreply.github.com>
2022-03-30 12:17:25 -0700

    flask basic development server

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-# This is a sample Python script.
+from flask import Flask
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'this is a Flask'
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app.run('127.0.0.1', port=8000, debug=True)
