d3fe179ba9f573169888626be9944a40abfee7e3
bond1999 <46317327+bond1999@users.noreply.github.com>
2022-03-30 13:04:15 -0700

    Deck GL implement added

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,32 +1,45 @@
 import os
+import pydeck
+from flask import Flask, render_template
+app = Flask(__name__)
 
-from flask import Flask
+# 2014 locations of car accidents in the UK
+UK_ACCIDENTS_DATA = ('https://raw.githubusercontent.com/uber-common/'
+                     'deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv')
+
+# Define a layer to display on a map
+layer = pydeck.Layer(
+    'HexagonLayer',
+    UK_ACCIDENTS_DATA,
+    get_position='[lng, lat]',
+    auto_highlight=True,
+    elevation_scale=50,
+    pickable=True,
+    elevation_range=[0, 3000],
+    extruded=True,
+    coverage=1)
+
+# Set the viewport location
+view_state = pydeck.ViewState(
+    longitude=-1.415,
+    latitude=52.2323,
+    zoom=6,
+    min_zoom=5,
+    max_zoom=15,
+    pitch=40.5,
+    bearing=-27.36)
+
+# Render
+r = pydeck.Deck(layers=[layer], initial_view_state=view_state)
+r.to_html('templates/demo.html')
+
+@app.route('/')
+def home():
+   return render_template('flaskr/demo.html')
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+if __name__ == '__main__':
+   app.run()
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
-
-    return app
+# Please see the note about using a Mapbox API token here:
+# https://github.com/uber/deck.gl/tree/master/bindings/python/pydeck#mapbox-api-token
