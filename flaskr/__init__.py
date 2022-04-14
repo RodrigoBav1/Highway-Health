710738c19d2d846530cfc27a667f50c1bf8fec1b
bond1999 <46317327+bond1999@users.noreply.github.com>
2022-04-14 17:13:24 -0500

    RGB visualized based on Incident Criticality

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,8 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # 2014 locations of car accidents in the UK
-TexasTrafficIncidents = ('traffic_incidents.csv')
-print(TexasTrafficIncidents)
+TexasTrafficIncidents = ('https://raw.githubusercontent.com/RodrigoBav1/Highway-Health/Surya/flaskr/traffic_incidents.csv')
 
 # Define a layer to display on a map
 layer = pydeck.Layer(
@@ -13,9 +12,17 @@ layer = pydeck.Layer(
     TexasTrafficIncidents,
     get_position='[GEOLOC_ORIGIN_LONGITUDE, GEOLOC_ORIGIN_LATITUDE]',
     auto_highlight=True,
-    get_radius=1000,  # Radius is given in meters
-    get_fill_color=[180, 0, 200, 140],  # Set an RGBA value for fill
+    radius_scale=1,
+    radius_min_pixels=5,
+    radius_max_pixels=5,
+    get_radius=1,  # Radius is given in meters
+    get_fill_color=['CRITICALITY_DESCRIPTION == "minor" ? 0 : CRITICALITY_DESCRIPTION == "major" ? 255 : 0', 'CRITICALITY_DESCRIPTION == "minor" ? 204 : CRITICALITY_DESCRIPTION == "major" ? 0 : 128', 'CRITICALITY_DESCRIPTION == "minor" ? 0 : CRITICALITY_DESCRIPTION == "major" ? 0 : 255', 128],  # Set an RGBA value for fill
     pickable=True)
+
+255, 255, 70
+255, 140, 70
+255, 0, 70
+
 
 # Set the viewport location
 view_state = pydeck.ViewState(
