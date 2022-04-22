78034486f0f91f031f42dc3847cb7e02dbd3b55e
bond1999 <surya.modern@gmail.com>
2022-04-22 13:11:44 -0500

    Merged weather layer into main

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,11 @@
-import os
 import pydeck
+import pandas
+import math
 from flask import Flask, render_template
 app = Flask(__name__)
 
 # 2014 locations of car accidents in the UK
-TexasTrafficIncidents = ('https://raw.githubusercontent.com/RodrigoBav1/Highway-Health/Surya/flaskr/traffic_incidents.csv')
+TexasTrafficIncidents = ('https://raw.githubusercontent.com/RodrigoBav1/Highway-Health/main/flaskr/traffic_incidents.csv')
 
 # Define a layer to display on a map
 layer = pydeck.Layer(
@@ -19,7 +20,80 @@ layer = pydeck.Layer(
     get_fill_color=['CRITICALITY_DESCRIPTION == "minor" ? 0 : CRITICALITY_DESCRIPTION == "major" ? 255 : 0', 'CRITICALITY_DESCRIPTION == "minor" ? 204 : CRITICALITY_DESCRIPTION == "major" ? 0 : 128', 'CRITICALITY_DESCRIPTION == "minor" ? 0 : CRITICALITY_DESCRIPTION == "major" ? 0 : 255', 128],  # Set an RGBA value for fill
     pickable=True)
 
+# Weather URL
+WEATHER_DATA_URL = "https://raw.githubusercontent.com/RodrigoBav1/Highway-Health/HH-Task-B-2/OpenWeatherMapsOutput.csv"
 
+# Create specialized icon object
+def get_icon_image_id(id, date_time):
+    #Decode weather ID
+    MSD = math.floor(id/100)                     #Most Significant Digit
+    SSD = math.floor((id%100)/10)    #Middle digit
+    LSD = id%10                      #Least Significant Digit
+
+    png_id = "02" #Cloudy icon default value
+
+    if(MSD == 2):
+       png_id = "11"
+    elif(MSD == 3):
+        png_id = "09"
+    elif(MSD == 5):
+        if(SSD == 0):
+            png_id = "10"
+        elif(SSD == 1):
+            png_id = "13"
+        elif(SSD == 2 or SSD == 3):
+            png_id = "09"
+    elif(MSD == 6):
+        png_id = "13"
+    elif(MSD == 7):
+        png_id = "50"
+    elif(MSD == 8):
+        if(LSD == 0):
+            png_id = "01"
+        elif(LSD == 1):
+            png_id = "02"
+        elif(LSD == 2):
+            png_id = "03"
+        elif(LSD == 3 or LSD == 4):
+            png_id == "04"
+
+    #Get night/day image
+    time = date_time.split(" ")
+    hour = time[1].split(":")
+
+    time_id = "d" #Day icon default value
+
+    #Night icon from 8 pm to 7 am
+    if(int(hour[0]) > 19 or int(hour[0]) < 7):
+        time_id = "n"
+
+    icon_id = png_id + time_id
+    icon_id = "02d"
+    icon_data = {
+        "url": "http://openweathermap.org/img/wn/" + icon_id + "@2x.png",
+        "width": 100,
+        "height": 100,
+        "anchorY": 100,
+    }
+    print(icon_data)
+    return icon_data
+
+# Append icon data to csv file
+data = pandas.read_csv(WEATHER_DATA_URL)
+data["icon_data"] = None
+for i in data.index:
+    data["icon_data"][i] = get_icon_image_id(data.iloc[i]["WEATHER_ID"], data.iloc[i]["DATE_TIME_CST"])
+
+# Define IconLayer
+iconLayer = pydeck.Layer(
+    type="IconLayer",
+    data=data,
+    get_icon="icon_data",
+    get_size=4,
+    size_scale=15,
+    get_position=["LONGITUDE", "LATITUDE"],
+    pickable=True,
+)
 
 # Set the viewport location
 view_state = pydeck.ViewState(
@@ -32,7 +106,7 @@ view_state = pydeck.ViewState(
     bearing=0)
 
 # Render
-r = pydeck.Deck(layers=[layer], map_style='road', initial_view_state=view_state)
+r = pydeck.Deck(layers=[layer, iconLayer], map_style='road', initial_view_state=view_state)
 r.to_html('templates/demo.html')
 
 @app.route('/')
